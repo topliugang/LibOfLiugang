fb70e7cd5f9b0ad2c6f2d765ad81bd74912eb40d
topliugang <335762225@qq.com>
2017-02-07 16:15:55 +0800

    2hand car

--- fuck2handcar.py
+++ fuck2handcar.py
@@ -1,53 +1,62 @@
+ # -*- coding:utf-8 -*- 
+
 import requests
 from bs4 import BeautifulSoup
 import json
 
+#修改python的defaultcoding至utf-8来解决写入unicode到文件的报错
+import sys
+reload(sys)                         
+sys.setdefaultencoding('utf-8')    
 
+base_url = 'http://www.che168.com'
+main_list_url = 'http://www.che168.com/china/list/'
+car_make_list_url = 'http://www.che168.com/handler/usedcarlistv5.ashx?action=brandlist&area=china&brand=&ls=&spec=&minPrice=0&maxPrice=0&minRegisteAge=0&maxRegisteAge=0&MileageId=0&disp=&stru=&gb=0&color=&source=0&listview=0&sell=1&newCar=0&credit=0&sort=0&page=1&ex=cd0t0p0w0ru0e0sa1o0i0g0nfzlk0v0a0b0m0_0y0x0h'
+
+#返回一个json字符串
+#return [{'name':name, 'url':url}]
+def get_car_make_list():
+	r = requests.get(car_make_list_url)
+	car_make_list = []
+	for car_make in json.loads(r.text):
+		#car_make_id = car_make['id']
+		#car_make_letter = car_make['letters']
+		name = car_make['name']
+		#car_make_pinyin = car_make['pinyin']
+		#seriesurl = car_make['seriesurl']
+		url = car_make['url']
+		car_make_list.append({'name':name, 'url':url})
+	return car_make_list
+
+#从页面抓取model list
+#return [{'name':car_model_name, 'url':car_model_url, 'type':car_type_name}]
+def get_car_model_list(car_make_name, car_make_url):
+	r = requests.get(car_make_url)
+	soup = BeautifulSoup(r.text, 'html.parser')
+	car_type_block_tag = soup.find('div', class_='condition-list fn-clear fn-hide', id='series')
+	car_model_list = []
+	if car_type_block_tag:
+		car_types_tag = car_type_block_tag.find_all('dl', class_='model-list fn-clear')
+		for car_type_tag in car_types_tag:
+			car_type_name = car_type_tag.find('dt').text
+			for car_model_tag in car_type_tag.find_all('a'):
+				car_model_url = car_model_tag['href']
+				car_model_name = car_model_tag.text
+				car_model_list.append({'name':car_model_name, 'url':car_model_url, 'type':car_type_name})
+	return car_model_list
+	
 
 def main():
-	base_url = 'http://www.che168.com'
-	main_list_url = 'http://www.che168.com/china/list/'
-	brand_list_url = 'http://www.che168.com/handler/usedcarlistv5.ashx?action=brandlist&area=china&brand=&ls=&spec=&minPrice=0&maxPrice=0&minRegisteAge=0&maxRegisteAge=0&MileageId=0&disp=&stru=&gb=0&color=&source=0&listview=0&sell=1&newCar=0&credit=0&sort=0&page=1&ex=cd0t0p0w0ru0e0sa1o0i0g0nfzlk0v0a0b0m0_0y0x0h'
-
-	r = requests.get(brand_list_url)
-	# html_file = open('brand_list.html', 'w')
-	# html_file.write(r.text)
-	# html_file.close()
-	# html = open('brand_list.html', 'r').read()
-	brand_list_json_str = r.text
-	brand_list_json_content = json.loads(brand_list_json_str)
-
-	#brand_list_json_content is list
-	for brand in brand_list_json_content:
-		brand_id = brand['id']
-		brand_letter = brand['letters']
-		brand_name = brand['name']
-		brand_pinyin = brand['pinyin']
-		brand_seriesurl = brand['seriesurl']
-		brand_url = brand['url']
-
-		
-		brand_full_url = base_url+brand_url
-		r = requests.get(brand_full_url)
-
-
-		brand_soup = BeautifulSoup(r.text, 'html.parser')
-		chexi = brand_soup.find('div', class_='condition-list fn-clear fn-hide', id='series')
-		chexings = chexi.find_all('dl', class_='model-list fn-clear')
-		for chexing in chexings:
-			chexing_class_name = chexing.find('dt').text
-			print chexing_class_name
-			for chexing_a in chexing.find_all('a'):
-				chexing_url = chexing_a['href']
-				chexing_name = chexing_a.text
-				chexing_full_url = base_url+chexing_url
-				print chexing_name
-				print chexing_full_url
-
-				r = requests.get(chexing_full_url)
-				html_file = open('htmls/fuck%s.html'%chexing_name, 'w')
-				html_file.write(r.text)
-				html_file.close()
+	for car_make in get_car_make_list():
+		car_make_name = car_make['name']
+		car_make_sub_url = car_make['url']
+		car_make_url = base_url+car_make_sub_url
+		for car_model in get_car_model_list(car_make_name, car_make_url):
+			print car_model['name']
+			print car_model['url']
+			print car_model['type']
+			print'----------'
+			print
 
 
 def fuck(url):
@@ -59,7 +68,7 @@ def fuck(url):
 
 
 if __name__ == '__main__':
-	#main()
-	fuck('http://www.che168.com/china/aodi/aodia4/#pvareaid=100512')
+	main()
+
 
 
